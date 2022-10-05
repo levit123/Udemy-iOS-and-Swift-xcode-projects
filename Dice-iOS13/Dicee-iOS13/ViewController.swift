92e40cf08ad5dbdc97087bceb4446c622dc44f80
levit123 <LeviCaseBlodgett@gmail.com>
2022-10-04 17:34:36 -0700

    working on Udemy course, section 4, part "Let's link our design to our code"

--- Dice-iOS13/Dicee-iOS13/ViewController.swift
+++ Dice-iOS13/Dicee-iOS13/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
